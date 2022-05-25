22356bc44cbb2363895d52119f21e7408d1340cb
chenym <loseyyou@gmail.com>
2022-05-25 09:38:30 +0800

    add

--- 1 File handle/File handle text/file handle 12 length of line in text file.py
+++ 1 File handle/File handle text/file handle 12 length of line in text file.py
@@ -1,6 +1,6 @@
 def longlines():
     F = open("story.txt", "r")
-    line = F.readlines()
+    line = F.readlines()            #按行读取
 
     for i in line:
         if len(i) < 50:
--- 1 File handle/File handle text/input,output and error streams.py
+++ 1 File handle/File handle text/input,output and error streams.py
@@ -2,19 +2,15 @@
 import sys
 
 sys.stdout.write("Enter the name of the file")
+
+#sys.stdout是python中的标准输出流，默认是映射到控制台的，即将信息打印到控制台。
 file = sys.stdin.readline()
-
+print(file)
 F = open(file.strip(), "r")
-
+print(F.readlines())
 while True:
     ch = F.readlines()
-    for (
-        i
-    ) in (
-        ch()
-    ):  # ch is the whole file,for i in ch gives lines, for j in i gives letters,for j in i.split gives words
+    for i in ch:
         print(i, end="")
-    else:
-        sys.stderr.write("End of file reached")
-        break
+
 F.close()
